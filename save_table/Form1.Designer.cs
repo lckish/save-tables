7e6848a21a208050c9b91f0d1a670049b7858218
lckish <92448931+lckish@users.noreply.github.com>
2022-04-28 20:27:03 +0400

    save table
    save table

---
+++ save_table/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace save_table
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileBTN = new System.Windows.Forms.Button();
+            this.saveInFileBTN = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileBTN
+            // 
+            this.openFileBTN.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.openFileBTN.Location = new System.Drawing.Point(8, 7);
+            this.openFileBTN.Margin = new System.Windows.Forms.Padding(2);
+            this.openFileBTN.Name = "openFileBTN";
+            this.openFileBTN.Size = new System.Drawing.Size(106, 26);
+            this.openFileBTN.TabIndex = 0;
+            this.openFileBTN.Text = "Открыть файл";
+            this.openFileBTN.UseVisualStyleBackColor = false;
+            this.openFileBTN.Click += new System.EventHandler(this.openFileBTN_Click);
+            // 
+            // saveInFileBTN
+            // 
+            this.saveInFileBTN.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.saveInFileBTN.Location = new System.Drawing.Point(127, 7);
+            this.saveInFileBTN.Margin = new System.Windows.Forms.Padding(2);
+            this.saveInFileBTN.Name = "saveInFileBTN";
+            this.saveInFileBTN.Size = new System.Drawing.Size(122, 26);
+            this.saveInFileBTN.TabIndex = 1;
+            this.saveInFileBTN.Text = "Сохранить в файл";
+            this.saveInFileBTN.UseVisualStyleBackColor = false;
+            this.saveInFileBTN.Click += new System.EventHandler(this.saveInFileBTN_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 37);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(493, 600);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(515, 648);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.saveInFileBTN);
+            this.Controls.Add(this.openFileBTN);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Сохранение таблицы";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileBTN;
+        private System.Windows.Forms.Button saveInFileBTN;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ save_table/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace save_table
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileBTN_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog.FileName;
+            // читаем файл в строку
+            try
+            {
+                string fileText = File.ReadAllText(filename);
+                //MessageBox.Show("Файл открыт");
+                DataTable dt = new DataTable();
+                using (StreamReader sr = new StreamReader(filename, Encoding.Default))
+                {
+                    string[] headers = sr.ReadLine().Split(';');
+                    foreach (string header in headers)
+                    {
+                        dt.Columns.Add(header);
+                    }
+                    while (!sr.EndOfStream)
+                    {
+                        string[] rows = sr.ReadLine().Split(';');
+                        DataRow dr = dt.NewRow();
+                        if (rows.Count() < headers.Count())
+                        {
+                            dr[headers.Count() - 1] = rows[0];
+                        }
+                        else
+                        {
+                            for (int i = 0; i < headers.Length; i++)
+                            {
+                                dr[i] = rows[i];
+                            }
+                        }
+                       
+                        dt.Rows.Add(dr);
+                    }
+
+                }
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void saveInFileBTN_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV files(*.csv)|*.csv|All files(*.*)|*.*|txt files(*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = saveFileDialog.FileName;
+            // сохраняем текст в файл
+            FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+            StreamWriter streamWriter = new StreamWriter(fs);
+            
+            try
+            {
+
+                for (int k = 0; k < dataGridView1.Columns.Count; k++)
+                {
+                    streamWriter.Write(dataGridView1.Columns[k].HeaderText.ToString() );
+                    if (k < dataGridView1.Columns.Count - 1) streamWriter.Write(";");
+                }
+                streamWriter.WriteLine();
+                for (int j = 0; j < dataGridView1.Rows.Count-1; j++)
+                {
+                    for (int i = 0; i < dataGridView1.Rows[j].Cells.Count; i++)
+                    {
+                        streamWriter.Write(dataGridView1.Rows[j].Cells[i].Value);
+                        if(i < dataGridView1.Rows[j].Cells.Count-1) streamWriter.Write(";");
+                    }
+
+                    streamWriter.WriteLine();
+                }
+
+                streamWriter.Close();
+                fs.Close();
+
+                MessageBox.Show("Файл успешно сохранен");
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при сохранении файла!");
+            }
+        }
+    }
+}
